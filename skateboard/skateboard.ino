b4f8fc846e961b399e6881abfda148517327c64c
aaroexxt <aaron.becker@gmail.com>
2019-11-29 18:39:07 -0800

    kinda sus half update

--- skateboard/skateboard.ino
+++ skateboard/skateboard.ino
@@ -31,6 +31,12 @@ ServoTimer2 ESC_RIGHT; //Create FSESC "servo" output
 #define ESC_R_PIN 5
 #define ESC_L_PIN 6
 
+#define ESC_MIN 800
+#define ESC_MAX 2000
+#define ESC_STOP (ESC_MIN+ESC_MAX)/2;
+float realPPM = ESC_STOP;
+float targetPPM = ESC_STOP;
+
 //Relay pins
 #define RELAY_PIN0 9
 #define RELAY_PIN1 10
@@ -43,10 +49,29 @@ ServoTimer2 ESC_RIGHT; //Create FSESC "servo" output
 CRGB leds[NUM_LEDS];
 #define LED_BRIGHTNESS 96
 #define LED_FPS 120
+uint8_t gHue = 0; // rotating "base color"
+int ledPosition = 0; //current position in strip for pattern
+int ledState = 0;
+/*
+0 initial or disconnect leds (chasing blue)
+1 rainbow
+2 follow throttle
+*/
 
 //Radio pins/defs
 RF24 radio(7, 8);
 const byte addresses [][6] = {"00001", "00002"}; //write at addr 00002, read at addr 00001
+//Send 6 bytes (because each int is 2 bytes) per rx/tx
+/*Data structure:
+First int is command number
+Second int is value 1
+Third int is value 2 (so you can send up to four bytes of data if you want)
+*/
+int dataRx[3];
+int dataTx[3];
+unsigned long prevHBMillis = 0;
+const int HBTimeoutMax = 4000; //max time between signals before board cuts the motors
+boolean radioListening = true;
 
 //IMU pins/defs
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
@@ -62,8 +87,8 @@ void setup() {
   //Setup ESC
   ESC_LEFT.attach(ESC_L_PIN);
   ESC_RIGHT.attach(ESC_R_PIN);
-  ESC_LEFT.write((ESC_MIN+ESC_MAX)/2); //set them to be basically off (middle position)
-  ESC_RIGHT.write((ESC_MIN+ESC_MAX)/2);
+  ESC_LEFT.write(realPPM); //set them to be basically off (middle position)
+  ESC_RIGHT.write(realPPM);
   Serial.println("Setup esc: ok");
 
   //Setup LEDS
@@ -88,10 +113,11 @@ void setup() {
   //Setup radio
   radio.begin();
   radio.setPALevel(RF24_PA_MAX); //max because we don't want to lose connection
+  radio.setRetries(3,3); // delay, count
   //SKATEBOARD Writes to addr 2, reads from addr 1
   radio.openWritingPipe(addresses[1]);
   radio.openReadingPipe(1, addresses[0]); //set address to recieve data
-  radio.startListening(); //puts radio in listening mode (waiting for hb signal from controller)
+  radioRecieveMode();
   Serial.println("Setup radio: ok");
 
 
@@ -105,6 +131,109 @@ void setup() {
 }
 
 void loop() {
+  switch (MASTER_STATE) {
+    case 0: //case 0 waiting for first hb signal from controller
+      radioRecieveMode(); //ensure we're recieving
+      dataRx = {0, 0, 0};
+      if (radio.available()) {
+        radio.read(&dataRx, sizeof(dataRx));
+        if (dataRx[0] == 200) { //200 is "heartbeat" signal
+          Serial.println("Got first heartbeat signal from controller");
+
+          radioTransmitMode();
+          dataTx = {200, 0, 0};
+          boolean ack = radio.send(&dataTx, sizeof(dataTx));
+          if (ack) { //did transmission go through?
+            Serial.println("hb ack first signal");
+            prevHBMillis = millis();
+            transitionState(1);
+          }
+
+        }
+      }
+    case 1: //standard operation
+      radioRecieveMode();
+      if (radio.available()) {
+        dataRx = {0, 0, 0};
+        radio.read(&dataRx, sizeof(dataRx));
+        Serial.println("Got event #: "+dataRx[0]+", value1: "+dataRx[1]+", value2: "+dataRx[2]);
+        switch (dataRx[0]) {
+          case 1: //1 is throttle update
+            targetPPM = dataRx[1];
+            break;
+          case 2: //2 is led update
+            if (dataRx[1] < 3) { //sanity check
+              ledState = dataRx[1];
+            }
+            break;
+          case 200: //heartbeat. if we get one, we should send one
+            Serial.println("controller hb recieved");
+            radioTransmitMode();
+            dataTx = {200, 0, 0};
+            boolean ack = radio.send(&dataTx, sizeof(dataTx));
+            if (!ack) {
+              transitionState(2); //state 2 is when we fail to get or send a heartbeat signal from the controller
+            } else {
+              prevHBMillis = millis();
+            }
+            break;
+        }
+      }
+      break;
+      case 2: //lost connection case
+        radioRecieveMode(); //ensure we're recieving
+        dataRx = {0, 0, 0};
+        if (radio.available()) {
+          radio.read(&dataRx, sizeof(dataRx));
+          if (dataRx[0] == 200) { //200 is "heartbeat" signal
+            Serial.println("Got heartbeat signal from controller after disconnection");
+
+            radioTransmitMode();
+            dataTx = {200, 0, 0};
+            boolean ack = radio.send(&dataTx, sizeof(dataTx));
+            if (ack) { //did transmission go through?
+              Serial.println("hb ack reconnect");
+              prevHBMillis = millis();
+              transitionState(1); //go back to normal operation
+            }
+          }
+        }
+        break;
+    default:
+      Serial.println("Undefined state; resetting");
+      transitionState(0);
+  }
+
+  switch (ledState) {
+    case 0: //blue chase
+      for (int i=0; i<NUM_LEDS; i++) {
+        if (i == ledPosition) {
+          leds[i] = CRGB::Blue;
+        } else {
+          leds[i] = CRGB::Black;
+        }
+      }
+      ledPosition++;
+      if (ledPosition > NUM_LEDS-1) {
+        ledPosition = 0;
+      }
+      break;
+    case 1: //rainbow
+      fill_rainbow(leds, NUM_LEDS, CRGB::Black, 7);
+      break;
+    case 2: //color changes based on throttle
+
+
+  }
+
+  unsigned long currentMillis = millis();
+  if (currentMillis-prevHBMillis>=HBTimeoutMax) { //have we lost the controller
+    transitionState(2);
+  }
+
+  updateESC();
+}
+
 
   //Sensor update code
   sensors_event_t event;
@@ -154,3 +283,31 @@ void loop() {
     Serial.println(curval);
 }
 
+void transitionState(int newState) {
+  MASTER_STATE = newState;
+  switch (newState) {
+    case 2: //uhoh we are going into remote disconnect mode
+      Serial.println("Uhoh we've lost connection to the remote :(");
+      ledState = 0; //go back into disconnected mode
+      targetPPM = ESC_STOP; //set target to 0 speed to bring us back down to 0 speed
+      break;
+  }  
+}
+
+void updateESCPercent(float percent) {
+  if ()
+}
+
+void radioRecieveMode() {
+  if (!radioListening) { //if we're not listening
+    radio.startListening();
+    radioListening = true;
+  }
+}
+
+void radioTransmitMode() {
+  if (radioListening) {
+    radio.stopListening();
+    radioListening = false;
+  }
+}
