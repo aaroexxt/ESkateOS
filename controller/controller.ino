a2f79411b0a8debb58bd17b89d314f5ee9553a1e
irosenba <ilan.rosenbaum@marklogic.com>
2021-08-17 18:57:32 -0700

    Typo

--- controller/controller.ino
+++ controller/controller.ino
@@ -292,7 +292,7 @@ void loop() {
             }
 
             // LEDs
-            int LEDReading = digitalRead(LED_BUTTON);  // Input pullup invert inputs (since it'll be pulled to ground if high)
+            int LEDReading = !digitalRead(LED_BUTTON);  // Input pullup invert inputs (since it'll be pulled to ground if high)
             if (LEDReading && (millis() - lastLEDDebounceTime) > debounceDelay) {
                 lastLEDDebounceTime = millis();
                 ledMode++;
@@ -313,7 +313,7 @@ void loop() {
             }
 
             // Turn Signals
-            int turnSignalReading = digitalRead(TURN_SIGNAL_BUTTON);  // Input pullup invert inputs (since it'll be pulled to ground if high)
+            int turnSignalReading = !digitalRead(TURN_SIGNAL_BUTTON);  // Input pullup invert inputs (since it'll be pulled to ground if high)
             if (turnSignalReading && (millis() - lastTSDebounceTime) > debounceDelay) {
                 lastTSDebounceTime = millis();
                 turnSignalMode++;
